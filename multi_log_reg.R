984bb966bae2682367e9176ba7bdc08c717ec27e
Tiago Siqueira <TSiqueira@FTancredi-MBP13.local>
2018-10-03 17:51:40 -0300

    uni to multi + correlation (interference)

--- multi_log_reg.R
+++ multi_log_reg.R
@@ -1,8 +1,31 @@
 library(readxl)
+library(PerformanceAnalytics)
 library(nnet)
-library()
 
+#Sets working directory for syncronization with GitHub
 setwd("/users/TSiqueira/Documents/trabalho2cm4")
 
+#Creates inital data frame fot T2
 initdf <- read_excel("BDLOGIST_GRUPO_11.xlsx")
 View(initdf)
+
+#Finding interaction between variables
+pairs(initdf[2:24])
+
+#### travail en cours
+chart.Correlation(initdf, method="pearson", histogram=TRUE, pch=16)
+
+#Multiple linear regression of all variables
+fitall <- lm(obito ~ ., data = initdf[2:24])
+summary(fitall)
+
+#Univariate linear regression for all variables 
+for (nayme in colnames(initdf)[2:24]) {
+  sprintf("%s ~ %s","fit obito", nayme) <- lm(obito ~ nayme, data = initdf[2:24])
+  summary(sprintf("%s ~ %s","fit obito", nayme))
+}
+
+
+fit <- lm(obito ~ sexo, data = initdf[2:24])
+summary(fit)
+
