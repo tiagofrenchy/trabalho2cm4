5c4e2ac5d98d703426143463a351e0e0477634f2
Tiago Siqueira <TSiqueira@FTancredi-MBP13.home>
2018-10-16 21:14:08 -0300

    split done

--- multi_log_reg.R
+++ multi_log_reg.R
@@ -14,16 +14,20 @@ View(initdf)
 
 #Finding interaction between variables
 pairs(initdf[2:24])
-
-#### travail en cours
 chart.Correlation(initdf, method="pearson", histogram=TRUE, pch=16)
 
+#Splitting the df in two to analyse separately after correlation identified
+splitdf <- split(initdf, initdf$sbdc)
+naosbdc <- splitdf[[1]]
+simsbdc <- splitdf[[2]]
+
+#Bartlett test for homocedasticity
 resultsListBart <- list()
 for (i in colnames(initdf)[2:23]) {
   #name <- sprintf("%s ~ %s","lmfit obito", i) 
-  fit <- bartlett.test(substitute(obito ~ i, list(i = as.name(i))),data = initdf)
-  resultsList[[i]] <- summary(fit)
-  print(summary(fit))
+  bart <- bartlett.test(substitute(obito ~ i, list(i = as.name(i))), data = initdf)
+  resultsList[[i]] <- summary(bart)
+  print(summary(bart))
 }
 
 #Univariate linear regression for all variables 
@@ -47,3 +51,11 @@ summary(fitselect)
 fitselect2 <- glm(obito ~ sbdc+bic_cat+tcec,family = binomial(logit) , data = initdf)
 summary(fitselect2)
 
+fitselect3 <- glm(obito ~ bic_cat+tcec,family = binomial(logit) , data = naosbdc)
+summary(fitselect3)
+
+fitselect4 <- glm(obito ~ bic_cat+tcec,family = binomial(logit) , data = simsbdc)
+summary(fitselect4)
+
+fitselect20 <- glm(obito ~ sbdc+bic_cat+tcec+biapos,family = binomial(logit) , data = initdf)
+summary(fitselect20)
